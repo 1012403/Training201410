d787318e4d263d85bb7bfdfda1c347e3902e98b3
vmthanh <luckyluck379@gmail.com>
2014-10-09 11:52:29 +0700

    JQuery - Half of complete

--- Topic2/my-jquery.js
+++ Topic2/my-jquery.js
@@ -1,4 +1,44 @@
 $(document).ready(function(){
+		//Create a number range [1..100]
+		var numRand1 = Math.floor(Math.random()*101);
+		var numRand2 = Math.floor(Math.random()*101);
+		$("#num1").val(numRand1);
+		$("#num2").val(numRand2);
+
+		function validationForm(){
+			var errorField = new Array();
+			if ($("#fullname").val() == ""){
+				errorField.push("fullname");
+			}
+			if ($("#company").val() == ""){
+				errorField.push("company");
+			}
+			if ($("#email").val() == ""){
+				errorField.push("email");
+			}
+			if ($("#message").val() == ""){
+				errorField.push("message");
+			}
+
+			var numRand3 = $("#num3").val();
+			if ((numRand1+numRand2) != numRand3){
+				errorField.push("number");
+			}
+
+			return errorField;
+
+		}
+		function provideFeedback(incomingErrors){
+			for (var i=0; i< incomingErrors.length; ++i){
+				$("#" + incomingErrors[i] + "Feedback").removeClass("errorFeedback");
+			}
+		}
+		function removeFeedback(){
+			$('.errorSpan').each(function(){
+				$(this).addClass("errorFeedback");
+			});
+		}
+
 		$("#searchId").on("click",function(){
 			$("#searchId").attr('value','');
 		});
@@ -11,14 +51,19 @@ $(document).ready(function(){
 		});
 
 		$("form.contact").submit(function(event){
-			if ($("#fullname").val() == ""){
-				$("#fullnameRequire").attr("visibility","visible");
-				alert("fail");
+		
+			removeFeedback();
+			var errors = validationForm();
+			if (errors == ""){
+				return true;
 			}
-			else
-			{
-				alert("Ok");
+			else{
+				provideFeedback(errors);
+				event.preventDefault();
+				return false;
 			}
-			return false;
-		})
+			
+		});
+		
+		
 });
