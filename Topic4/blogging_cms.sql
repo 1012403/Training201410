925612662275a478c62d93414df1fb75b32119fb
origin <khoahoang06@gmail.com>
2014-10-27 00:18:12 +0700

    ud

--- Topic4/blogging_cms.sql
+++ Topic4/blogging_cms.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 25, 2014 at 03:58 AM
+-- Generation Time: Oct 26, 2014 at 06:17 PM
 -- Server version: 5.6.20
 -- PHP Version: 5.5.15
 
@@ -32,14 +32,26 @@ CREATE TABLE IF NOT EXISTS `comments` (
   `user_id` int(11) NOT NULL,
   `comment` text COLLATE utf8_unicode_ci NOT NULL,
   `comment_time` datetime NOT NULL
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=2 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=87 ;
 
 --
 -- Dumping data for table `comments`
 --
 
 INSERT INTO `comments` (`comment_id`, `post_id`, `user_id`, `comment`, `comment_time`) VALUES
-(1, 14, 34, 'khoa Ä‘áº¡i ca', '2014-10-25 08:09:32');
+(73, 48, 34, 'ngon nÃ¨', '2014-10-25 10:30:19'),
+(74, 48, 34, 'Ã½e', '2014-10-25 10:30:32'),
+(75, 49, 34, 'how', '2014-10-25 10:32:42'),
+(76, 49, 34, 'alo', '2014-10-25 10:38:20'),
+(77, 50, 34, 'nÃ¨', '2014-10-25 10:38:46'),
+(78, 53, 34, 'dddd', '2014-10-25 10:45:41'),
+(79, 53, 34, 'demo', '2014-10-25 10:45:45'),
+(80, 52, 34, 'demo', '2014-10-25 10:45:58'),
+(81, 52, 34, '', '2014-10-25 10:45:58'),
+(82, 57, 34, 'Demo', '2014-10-25 10:50:55'),
+(84, 59, 35, 'hi khoa hoang', '2014-10-25 11:31:32'),
+(85, 60, 35, 'alo', '2014-10-25 11:46:25'),
+(86, 60, 35, 'alo', '2014-10-25 11:47:27');
 
 -- --------------------------------------------------------
 
@@ -50,29 +62,38 @@ INSERT INTO `comments` (`comment_id`, `post_id`, `user_id`, `comment`, `comment_
 CREATE TABLE IF NOT EXISTS `posts` (
 `post_id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
-  `post_content` text COLLATE utf8_unicode_ci NOT NULL,
+  `post_content` longtext COLLATE utf8_unicode_ci NOT NULL,
   `post_time` datetime NOT NULL,
   `user_post` int(11) NOT NULL
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=22 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=68 ;
 
 --
 -- Dumping data for table `posts`
 --
 
 INSERT INTO `posts` (`post_id`, `user_id`, `post_content`, `post_time`, `user_post`) VALUES
-(9, 34, 'h, thiÌ€ chiÌ‰ trong voÌ€ng 6 tuÃ¢Ì€n Ä‘aÌƒ... CoÌ€n laÌ£i 1 sÃ´Ì laÌ€ FA, Ä‘Æ°Æ¡ng nhiÃªn con sÃ´Ì naÌ€ y ko thÃªÌ‰ naÌ€o chiÌnh xaÌc, viÌ€ coÌ thÃªÌ‰ nhiu', '2014-10-24 22:13:58', 34),
-(10, 34, 'Ã¡dasdasd', '2014-10-24 22:44:04', 34),
-(11, 34, 'acdb', '2014-10-24 22:46:09', 34),
-(12, 34, 'laylist Tuyá»ƒn táº­p cÃ¡c ca khÃºc US-UK hay :) do ca sÄ© Äang Cáº­p Nháº­t thá»ƒ hiá»‡n, thuá»™c thá»ƒ loáº¡i Playlist Ã‚u, Má»¹. CÃ¡c báº¡n cÃ³ thá»ƒ nghe, download (táº£i nháº¡c) cÃ¡c bÃ i hÃ¡t trong playlist/album Tuyen Tap CÃ¡c Ca KhÃºc Us-uk Hay :) mp3 miá»…n phÃ­ táº¡i NhacCuaTui.', '2014-10-24 22:48:53', 34),
-(13, 34, '123131323', '2014-10-24 22:56:02', 34),
-(14, 34, '34458667', '2014-10-25 08:03:44', 34),
-(15, 34, 'y do ca sÄ© P!nk thá»ƒ hiá»‡n, thuá»™c thá»ƒ loáº¡i Video Ã‚u, Má»¹, Video Pop. CÃ¡c báº¡n cÃ³ thá»ƒ nghe ,download MV/Video Try miá»…n phÃ­ táº¡i NhacCuaTui.com. ', '2014-10-25 08:12:14', 34),
-(16, 34, 'khoahoang', '0000-00-00 00:00:00', 34),
-(17, 34, 'khÃ³a', '2014-10-25 08:44:06', 34),
-(18, 34, 'zing', '2014-10-25 08:46:21', 34),
-(19, 34, 'kaka', '2014-10-25 08:54:06', 34),
-(20, 34, 'khsfasfsafasasfsdfsdfasdf', '2014-10-25 08:54:40', 34),
-(21, 34, 'lksfhkjsgkasfhlkasfhsaklfhkl', '2014-10-25 08:54:53', 34);
+(46, 34, 'alo', '2014-10-25 10:18:14', 34),
+(47, 34, 'alo ná»¯a', '2014-10-25 10:18:57', 34),
+(48, 34, 'thá»­ láº¡i', '2014-10-25 10:25:47', 34),
+(49, 34, 'ok nÃ¨', '2014-10-25 10:30:23', 34),
+(50, 34, 'thá»­ láº¡i', '2014-10-25 10:38:24', 34),
+(51, 34, 'ok', '2014-10-25 10:38:50', 34),
+(52, 34, 'Demov ', '2014-10-25 10:44:29', 34),
+(53, 34, 'demo ', '2014-10-25 10:45:33', 34),
+(54, 34, 'Demo\n', '2014-10-25 10:47:00', 34),
+(55, 34, 'Dmo2222 ', '2014-10-25 10:47:54', 34),
+(56, 34, 'Demo \n', '2014-10-25 10:50:11', 34),
+(57, 34, 'Demo de', '2014-10-25 10:50:51', 34),
+(58, 34, '', '2014-10-25 10:52:38', 34),
+(59, 34, '', '2014-10-25 10:52:58', 34),
+(60, 35, 'bÃ i cá»§a luffy', '2014-10-25 11:31:10', 35),
+(61, 35, 'Salvia portland leggings banh mi fanny pack mixtape, authentic bushwick wes anderson intelligentsia artisan typewriter high life they sold out mixtape high life. Marfa ethnic wayfarers brooklyn keytar mixtape. Blue bottle shoreditch gluten-free, mixtape hoodie whatever pinterest viral twee fashion axe high life irony biodiesel tofu.\n\nRetro church-key thundercats keytar, skateboard irony selvage ethnic freegan banjo pour-over fixie. Raw denim fashion ax eoke locavore disrupt, tonx retro authentic letterpress raw denim stumptown mixtape ugh ', '2014-10-25 13:00:41', 35),
+(62, 35, 'Ã¡ds', '2014-10-25 13:12:27', 35),
+(63, 35, 'Skateboard artisan bicycle rights next level vinyl cardigan beard twee, farm-to-table truffaut. Shoreditch freegan cliche thundercats, bushwick VHS intelligentsia selfies ethnic try-hard before they sold out. Marfa terry richardson hella, seitan odd future pug butcher. Wes anderson tousled YOLO cardigan. Typewriter high life carles, artisan gentrify mess', '2014-10-25 13:17:48', 35),
+(64, 35, 'Skateboard artisan bicycle rights next level vinyl cardigan beard twee, farm-to-table truffaut. Shoreditch freegan cliche thundercats, bushwick VHS intelligentsia selfies ethnic try-hard before they sold out. Marfa terry richardson hella, seitan odd future pug butcher. Wes anderson tousled YOLO cardigan. Typewriter high life carles, artisan gentrify messe', '2014-10-25 13:18:57', 35),
+(65, 35, 'Skateboard artisan bicycle rights next level vinyl cardigan beard twee, farm-to-table truffaut. Shoreditch freegan cliche thundercats, bushwick VHS intelligentsia selfies ethnic try-hard before they sold out. Marfa terry richardson hella, seitan odd future pug butcher. Wes anderson tousled YOLO cardigan. Typewriter high life carles, artisan gentrify messenger ', '2014-10-25 13:19:07', 35),
+(66, 35, 'Skateboard artisan bicycle rights next level vinyl cardigan beard twee, farm-to-table truffaut. Shoreditch freegan cliche thundercats, bushwick VHS intelligentsia selfies ethnic try-hard before they sold out. Marfa terry richardson hella, seitan odd future pug butcher. Wes anderson tousled YOLO cardigan. y', '2014-10-25 13:21:01', 35),
+(67, 35, 'Skateboard artisan bicycle rights next level vinyl cardigan beard twee, farm-to-table truffaut. Shoreditch freegan cliche thundercats, bushwick VHS intelligentsia selfies ethnic try-hard before they sold out. Marfa terry richardson hella, seitan odd future pug butcher. Wes anderson tousled YOLO cardigan. Typewriter high life carles, artisan gentrify messenger bag single-origin coffee truffaut thundercats cray', '2014-10-25 13:21:08', 35);
 
 -- --------------------------------------------------------
 
@@ -87,14 +108,15 @@ CREATE TABLE IF NOT EXISTS `users` (
   `password` varchar(40) COLLATE utf8_unicode_ci NOT NULL,
   `active` varchar(60) COLLATE utf8_unicode_ci DEFAULT NULL,
   `registration_date` datetime NOT NULL
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=35 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=36 ;
 
 --
 -- Dumping data for table `users`
 --
 
 INSERT INTO `users` (`user_id`, `username`, `email`, `password`, `active`, `registration_date`) VALUES
-(34, 'khoahoang', 'khoahoang06@gmail.com', '40bd001563085fc35165329ea1ff5c5ecbdbbeef', NULL, '2014-10-23 19:39:11');
+(34, 'khoahoang', 'khoahoang06@gmail.com', '40bd001563085fc35165329ea1ff5c5ecbdbbeef', NULL, '2014-10-23 19:39:11'),
+(35, 'Luffy', 'hdkcr7@gmail.com', '40bd001563085fc35165329ea1ff5c5ecbdbbeef', NULL, '2014-10-25 11:29:33');
 
 --
 -- Indexes for dumped tables
@@ -126,17 +148,17 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `comments`
 --
 ALTER TABLE `comments`
-MODIFY `comment_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+MODIFY `comment_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=87;
 --
 -- AUTO_INCREMENT for table `posts`
 --
 ALTER TABLE `posts`
-MODIFY `post_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=22;
+MODIFY `post_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=68;
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=35;
+MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=36;
 --
 -- Constraints for dumped tables
 --
